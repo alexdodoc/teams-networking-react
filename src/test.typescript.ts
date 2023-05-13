dca321ce498acfc732bbfd2eca7efbe80948e2db
Dodoc Alexandru <alex.dodoc@yahoo.com>
2023-05-13 14:04:08 +0300

    change tsconfig

---
+++ src/test.typescript.ts
@@ -0,0 +1,15 @@
+function getConf() {
+  return {
+    name: "my conf",
+    x: 123,
+    ready: true
+  };
+}
+
+const conf = getConf();
+//console.info(conf.nume);
+
+function a(b) {
+  const c = 1;
+  return b * 2;
+}
