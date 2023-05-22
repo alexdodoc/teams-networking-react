193179bb94c7fc08aa1433790023fd87ed13c42f
Dodoc Alexandru <alex.dodoc@yahoo.com>
2023-05-22 20:11:50 +0300

    use this.save.bind(this)

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -191,10 +191,13 @@ export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
       team: getEmptyTeam()
     };
 
-    const originalSave = this.save;
-    this.save = async () => {
-      originalSave.call(this);
-    };
+    // const originalSave = this.save;
+    // this.save = async () => {
+    //   originalSave.call(this);
+    // };
+    this.save = this.save.bind(this);
+    this.deleteTeam = this.deleteTeam.bind(this);
+    this.inputChange = this.inputChange.bind(this);
   }
 
   componentDidMount(): void {
@@ -261,9 +264,7 @@ export class TeamsTableWrapper extends React.Component<WrapperProps, State> {
         startEdit={(team) => {
           this.setState({ team });
         }}
-        inputChange={(name: string, value: string) => {
-          this.inputChange(name, value);
-        }}
+        inputChange={this.inputChange}
       />
     );
   }
